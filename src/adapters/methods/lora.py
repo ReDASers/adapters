scaling fix

src/adapters/methods/lora.py
@@ -83,12 +83,12 @@ class LoRA(nn.Module):
                 if config.scaling is None or isinstance(config.scaling, float):
                     self.scaling = float(self.lora_alpha / math.sqrt(self.r))
                 elif  config.scaling == "learnable":
-                    self.scaling = nn.Parameter(torch.tensor(float(self.lora_alpha) / math.sqrt(float(self.r))), dtype=torch.float32, requires_grad=True)
+                    self.scaling = nn.Parameter(torch.tensor(float(self.lora_alpha) / math.sqrt(float(self.r)), dtype=torch.float32, requires_grad=True))
             else:
                 if config.scaling is None:
-                    self.scaling = 1.0
+                    self.scaling = torch.ones(1, dtype=torch.float32, requires_grad=False)
                 elif isinstance(config.scaling, float) or isinstance(config.scaling, int):
-                    self.scaling = torch.tensor(max(config.scaling, 1.0))
+                    self.scaling = torch.tensor(max(config.scaling, 1.0), dtype=torch.float32, requires_grad=False)
                 elif  config.scaling == "learnable":
                     self.scaling = nn.Parameter(torch.tensor(float(1.0), dtype=torch.float32, requires_grad=True))
         else: