051e5866b3fa1e23954efbb40077dc29f5f1a1e2
Dainis Boumber <dainis.boumber@gmail.com>
2024-06-27 02:19:07 +0000

    param print

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -53,9 +53,8 @@ class LoRA(nn.Module):
         self.bn_activation = config.bn_activation
         self.hidden_size_in = lora_A_shape[-1]
         self.num_weights_out = lora_A_shape[-1] if self.legacy else lora_B_shape[0]
-        for name, param in self.named_parameters():
-            print(name)
-            break
+
+            
         # Optional dropout
         if config.dropout > 0.0:
             self.lora_dropout = nn.Dropout(p=config.dropout)
@@ -147,6 +146,13 @@ class LoRA(nn.Module):
             nn.init.ones_(self.lora_C)
         else:
             raise ValueError("Unknown init_weights type: {}".format(config.init_weights))
+        
+        i = 0
+        for name, param in self.named_parameters():
+            i += 1
+            print("param: ", name)
+            if i == 3:
+                break
 
         if self.use_gating:
             self.gate = nn.Linear(lora_A_shape[-1], gating_heads)
