fix(lora): adjust normalization in dense_fan_in mode
Changed the normalization method in the dense_fan_in mode to use both L1 and L2 norms, ensuring more stable training and better handling of outliers. This adjustment aims to improve model robustness and performance.

src/adapters/methods/lora.py
@@ -371,8 +371,9 @@ class LoRA(nn.Module):
                 hidden_states = torch.nan_to_num(hidden_states)
                 hidden_states = hidden_states * scaling_vector
             if self.mode == "dense_fan_in":
-                norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
-                hidden_states = hidden_states / norm
+                l1_norm = hidden_states.norm(p=1, dim=1, keepdim=True) + 1e-9
+                l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
+                hidden_states = hidden_states / l1_norm / l2_norm
             else:
                 l1_norm = hidden_states.norm(p=1, dim=1, keepdim=True) + 1e-9
                 l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9