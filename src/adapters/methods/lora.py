eef0cd7c8668ccde4b9f03490a337a08193fe74d
Dainis Boumber <dainis.boumber@gmail.com>
2024-07-13 18:21:56 +0000

    l2+l1 norm corrected

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -355,11 +355,14 @@ class LoRA(nn.Module):
                 #l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
                 hidden_states = hidden_states# / l2_norm
             else: # if intermediate layer/fanout -> do elastic net
-                l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
+                
                 if self.elastic_net:
                     l1_norm = hidden_states.norm(p=1, dim=1, keepdim=True) + 1e-9
                     hidden_states = hidden_states / l1_norm 
+                    l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
+                    hidden_states = hidden_states / l2_norm
                 else:
+                    l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
                     hidden_states = hidden_states / l2_norm
 
                     
