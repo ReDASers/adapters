fix(lora): re-enable l2 normalization in dense_fan_in mode
Reinstated the l2 normalization for hidden states in dense_fan_in mode to ensure proper scaling. This change corrects an earlier commented-out code section, enhancing numerical stability.

src/adapters/methods/lora.py
@@ -352,15 +352,13 @@ class LoRA(nn.Module):
                 hidden_states = torch.nan_to_num(hidden_states)
                 hidden_states = hidden_states * scaling_vector
             if self.mode == "dense_fan_in":
-                #l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
-                hidden_states = hidden_states# / l2_norm
+                l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
+                hidden_states = hidden_states / l2_norm
             else: # if intermediate layer/fanout -> do elastic net
                 
                 if self.elastic_net:
                     l1_norm = hidden_states.norm(p=1, dim=1, keepdim=True) + 1e-9
-                    hidden_states = hidden_states / l1_norm 
-                    l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
-                    hidden_states = hidden_states / l2_norm
+                    hidden_states = hidden_states / l1_norm / l2_norm
                 else:
                     l2_norm = hidden_states.norm(p=2, dim=1, keepdim=True) + 1e-9
                     hidden_states = hidden_states / l2_norm