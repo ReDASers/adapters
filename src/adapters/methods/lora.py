fix(lora): adjust initialization standard deviation for lora_C
Increased the standard deviation for initializing `lora_C` from 0.01 to 0.02 to potentially enhance model performance and stability.

src/adapters/methods/lora.py
@@ -152,7 +152,7 @@ class LoRA(nn.Module):
         Sets up the basic calculation mode by initializing LoRA parameters.
         """
         self.lora_C = nn.Parameter(torch.zeros(self.num_weights_out, 1))
-        self.lora_C = nn.init.normal_(self.lora_C, mean=1.0, std=0.01)
+        self.lora_C = nn.init.normal_(self.lora_C, mean=1.0, std=0.02)
         #nn.init.ones_(self.lora_C)
 
     def _setup_full_calculation(self, lora_A_shape, lora_B_shape):