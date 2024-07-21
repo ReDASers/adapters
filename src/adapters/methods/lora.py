a9b9eb044f442245911d1f50bea7f5dd221c2a3a
Dainis Boumber <dainis.boumber@gmail.com>
2024-07-21 20:03:48 +0000

    refactor(lora): update scaling vector computation logic
    Revised the calculation of `scaling_vector` in cases where `scalar_fan_in` is not applied. The new implementation uses `scalar_fan_out` with ReLU6 activation, ensuring a non-negative scaling adjustment. This change improves numerical stability and consistency in scaling operations.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -373,8 +373,9 @@ class LoRA(nn.Module):
                 # Apply the positive scalar and ensure non-negative scaling vector
                 scaling_vector = scaling_vector * scalar_fan_in + self.eps
             else:
-                #scaling_vector = F.relu6(scaling_vector - self.eps) + 1e-12
-                pass
+                scalar_fan_out = F.relu6(self.scalar_fan_out) + self.eps
+                scaling_vector = F.relu6(scaling_vector/scalar_fan_out - self.eps)
+                
             #else:
             #    scaling_vector = scaling_vector * self.scalar_fan_out
             # If hidden_states is None, use scaling_vector instead - this is the case most of the time
