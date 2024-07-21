fix(lora): correct scaling vector calculation
Changed the scaling vector formula to improve stability by dividing by (1 + self.eps) instead of using relu6. This adjustment aims to provide more consistent results in edge cases where the previous method might have been unstable.

src/adapters/methods/lora.py
@@ -374,7 +374,7 @@ class LoRA(nn.Module):
                 scaling_vector = scaling_vector * scalar_fan_in + self.eps
                 
             else:
-                scaling_vector = F.relu6(scaling_vector + self.eps/20)
+                scaling_vector = scaling_vector/(1 + self.eps)
             # If hidden_states is None, use scaling_vector instead - this is the case most of the time
             
             if hidden_states is None: