fix(lora): correct scaling vector calculation with step multiplier
The previous implementation incorrectly rescaled the scaling vector without considering the step multiplier, leading to potential inaccuracies. This commit adjusts the scaling mechanism by incorporating the step multiplier, ensuring accurate and consistent rescaling throughout the training process.

src/adapters/methods/lora.py
@@ -360,7 +360,7 @@ class LoRA(nn.Module):
             Tuple[torch.Tensor, Optional[torch.Tensor]]: Processed hidden states and gate (if applicable).
         """
         self.n_steps += 1
-        epoch = self.n_steps // self.rescale_frequency
+        multiplier = self.n_steps % self.rescale_frequency
         # This may be a bit hard to follow because of optimizations
         # Check if full calculation mode is enabled
         if self.mode == "attention":
@@ -384,14 +384,14 @@ class LoRA(nn.Module):
                 if "scalar_fan_in" in self.dense_strategy or "scalar_both" in self.dense_strategy:
                     # Apply the positive scalar and ensure non-negative scaling vector
                    
-                    scaling_vector = scaling_vector * (1.0 + self.scalar_scaler)
+                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler*multiplier)
 
 
             elif self.mode == "dense_fan_out":
                 if "scalar_fan_out" in self.dense_strategy or "scalar_both" in self.dense_strategy:
                     # Apply the positive scalar and ensure non-negative scaling vector
                     
-                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler)
+                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler*multiplier)
 
                     
             