fix(lora): adjust scalar_scaler calculation by incorporating epoch
In the LoRA class, modified the scalar_scaler calculation to include the epoch variable, ensuring that the scaling factor changes over time. This adjustment affects both "scalar_fan_in" and "scalar_fan_out" modes within the dense_strategy.

src/adapters/methods/lora.py
@@ -384,14 +384,14 @@ class LoRA(nn.Module):
                 
                 if "scalar_fan_in" in self.dense_strategy or "scalar_both" in self.dense_strategy:
                     # Apply the positive scalar and ensure non-negative scaling vector
-                    scalar_scaler = 1.0 - self.scalar_scaler
+                    scalar_scaler = 1.0 - self.scalar_scaler * epoch
                     scaling_vector = scaling_vector * scalar_scaler
 
 
             elif self.mode == "dense_fan_out":
                 if "scalar_fan_out" in self.dense_strategy or "scalar_both" in self.dense_strategy:
                     # Apply the positive scalar and ensure non-negative scaling vector
-                    scalar_scaler = 1.0 - self.scalar_scaler
+                    scalar_scaler = 1.0 - self.scalar_scaler * epoch
                     scaling_vector = scaling_vector * scalar_scaler
 
                     