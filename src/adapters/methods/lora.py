a01a1791368b2de8eb62f5248276422105ac610a
Dainis Boumber <dainis.boumber@gmail.com>
2024-07-28 19:22:04 +0000

    fix(lora): remove redundant subtraction of epsilon from scaling vector
    Removed unnecessary subtraction of epsilon from the scaling vector calculation in `dense_fan_in` and `dense_fan_out` modes to ensure correct scaling.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -383,14 +383,14 @@ class LoRA(nn.Module):
                 if "scalar_fan_in" in self.dense_strategy or "scalar_both" in self.dense_strategy:
                     # Apply the positive scalar and ensure non-negative scaling vector
                    
-                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler) - self.eps
+                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler)
 
 
             elif self.mode == "dense_fan_out":
                 if "scalar_fan_out" in self.dense_strategy or "scalar_both" in self.dense_strategy:
                     # Apply the positive scalar and ensure non-negative scaling vector
                     
-                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler) - self.eps
+                    scaling_vector = scaling_vector * (1.0 - self.scalar_scaler)
 
                     
             
