fix(lora): correct sigma calculation for negative values
Changed the sigma calculation in _init_scaling_weights to use 0 instead of 1e-2 for negative sigma values. This ensures proper initialization and scaling of weights, potentially improving model performance and stability.

src/adapters/methods/lora.py
@@ -161,7 +161,7 @@ class LoRA(nn.Module):
 
     def _init_scaling_weights(self):
         if self.sigma < 0:
-            self.sigma =  math.sqrt(2 / ((1 + (1e-2) ** 2) * self.connections_in))
+            self.sigma =  math.sqrt(2 / ((1 + (0) ** 2) * self.connections_in))
         elif self.sigma == 0:
             self.sigma =  math.sqrt(2 / ((1 + (1e-2) ** 2) * self.connections_out))
         nn.init.normal_(self.lora_C, mean=1.0, std=self.sigma)