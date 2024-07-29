af20b73085c2d165ac9aac068b6db82300d30af5
Dainis Boumber <dainis.boumber@gmail.com>
2024-07-29 18:39:43 +0000

    refactor(lora): remove rescale function call in dense_fan_out mode
    The rescale function call in the dense_fan_out mode has been commented out, simplifying the code to directly use the added and scaling variables. This change might improve performance by reducing unnecessary computations.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -299,7 +299,8 @@ class LoRA(nn.Module):
         elif self.mode == "dense_fan_in": 
             return weights * (added * scaling)
         elif self.mode == "dense_fan_out":
-            return weights * (self.rescale(added, sigma=self.sigma) * scaling)
+            return weights * (added * scaling)
+            #return weights * (self.rescale(added, sigma=self.sigma) * scaling)
         elif self.mode == "noop":
             return weights
         else:
