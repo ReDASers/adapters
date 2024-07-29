fix(lora): correct sigma scaling logic and add dense_fan_out handling
Adjusted the sigma calculation to use sqrt(5) for both conditions when sigma is less than or equal to 0. Added a new case to handle dense_fan_out mode by adjusting sigma based on connections ratio. This ensures more accurate weight initialization in different configurations.

src/adapters/methods/lora.py
@@ -161,9 +161,13 @@ class LoRA(nn.Module):
 
     def _init_scaling_weights(self):
         if self.sigma < 0:
-            self.sigma =  math.sqrt(2 / ((1 + (0) ** 2) * self.connections_in))
+            self.sigma =  math.sqrt(2 / ((1 + (math.sqrt(5)) ** 2) * self.connections_in))
         elif self.sigma == 0:
-            self.sigma =  math.sqrt(2 / ((1 + (1e-2) ** 2) * self.connections_out))
+            self.sigma =  math.sqrt(2 / ((1 + (math.sqrt(5)) ** 2) * self.connections_out))
+        elif self.sigma > 0 and self.mode == "dense_fan_out":
+            self.sigma = self.sigma/(self.connections_out/self.connections_in)
+        else:
+            self.sigma = self.sigma
         nn.init.normal_(self.lora_C, mean=1.0, std=self.sigma)
             
     def _setup_in_attn(self, lora_A_shape, lora_B_shape):