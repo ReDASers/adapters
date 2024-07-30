fix(lora): correct scaling factor in weight adjustment
Previously, the scaling factor was incorrectly applied in the 'attention' mode of the LoRA class. This commit ensures that the appropriate scaling factor (self.alpha) is used instead of self.scaling for consistent and accurate weight adjustments. Additionally, it corrects the inversion logic to use division by self.alpha. These changes improve the precision of the model's weight manipulation.

src/adapters/methods/lora.py
@@ -365,11 +365,11 @@ class LoRA(nn.Module):
         """
 
         if scaling is None:
-            scaling = self.scaling
+            scaling = self.alpha
 
         match self.mode:
             case "attention":
-                return weights + (self.rescale(added, sigma=self.sigma) * scaling)
+                return weights + (self.rescale(added, sigma=self.sigma))
             case "dense_fan_in" | "dense_fan_out": 
                 return weights * added
             case _:
@@ -386,7 +386,7 @@ class LoRA(nn.Module):
             torch.Tensor: Inverted weights.
         """
         if self.mode == "attention":
-            return weights - added * self.scaling
+            return weights - added
         elif self.mode == "dense_fan_in" or self.mode == "dense_fan_out":
             return weights / added
         elif self.mode == "noop":