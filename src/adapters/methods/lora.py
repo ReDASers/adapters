fix(lora): correct scaling factor and initialization mode
Corrected the scaling factor in _estimate_scaling_sigma to use connections_out instead of connections_in. Also fixed the initialization mode for nn.Linear layers to fan_out with appropriate parameters, ensuring consistency and improving model performance.

src/adapters/methods/lora.py
@@ -257,7 +257,7 @@ class LoRA(nn.Module):
         nn.init.normal_(self.lora_C, mean=1.0, std=self.sigma)
 
     def _estimate_scaling_sigma(self):
-        return math.sqrt(2 / ((1 + (self._get_neg_slope(self.non_linearity)) ** 2) * self.connections_in))
+        return math.sqrt(2 / ((1 + (self._get_neg_slope(self.non_linearity)) ** 2) * self.connections_out))
        
 
     def _estimate_attn_sigma(self):
@@ -341,11 +341,9 @@ class LoRA(nn.Module):
 
         for layer in layers:
             if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight, mode="fan_out", a=self._get_neg_slope(self.non_linearity))
-                if layer.bias is not None:
-                    nn.init.zeros_(layer.bias)
+
                 #nn.init.kaiming_normal_(layer.weight, a=self._get_neg_slope(self.non_linearity), mode="fan_out", nonlinearity="leaky_relu")
-                nn.init.kaiming_normal_(layer.weight, mode="fan_in", a=math.sqrt(5))
+                nn.init.kaiming_normal_(layer.weight, mode="fan_out", a=math.sqrt(5))
                 
                 if layer.bias is not None:
                     nn.init.zeros_(layer.bias)