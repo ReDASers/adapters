fix(lora): adjust sigma_w calculation for epoch-based training
Updated the sigma_w calculation to be based on epochs instead of training steps. This change ensures that sigma_w is correctly averaged over all batches in an epoch, improving the stability and accuracy of weight rescaling during training.

src/adapters/methods/lora.py
@@ -423,9 +423,10 @@ class LoRA(nn.Module):
         Returns:
             torch.Tensor: Composed weights.
         """
-        if self.training and self.training_steps == 1:
-            self.sigma_w = weights.std().item()
-        # burn in period
+        if self.training and self.epoch == 1:
+            self.sigma_w = self.sigma_w + weights.std().item()
+            if self._epoch_end():
+                self.sigma_w = self.sigma_w / self.batches_per_epoch
         
         if self._epoch_start() and self.epoch > 1:
             w = self.rescale(weights, self.sigma_w)