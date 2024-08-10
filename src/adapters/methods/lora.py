887cde2a51d4e34b88b7319e5242b5d2de04c298
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-10 19:06:15 +0000

    fix(lora): adjust threshold for delta_w rescaling
    Increased the threshold for rescaling delta_w from 3% to 5% of sigma_h to improve stability in weight updates. This change ensures that rescaling occurs less frequently, potentially reducing unnecessary adjustments and improving model performance.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -526,7 +526,7 @@ class LoRA(nn.Module):
             self.record_var(dw_std, "dw_std")
             self.record_var(self.sigma_h, "dw_norm")
             # Rescale delta_w if its standard deviation is greater than sigma_h
-            if dw_std > self.sigma_h + self.sigma_h * 0.03:
+            if dw_std > self.sigma_h + self.sigma_h * 0.05:
                 hidden_states = self.rescale(normed_dw, self.sigma_h)
             elif dw_std < self.sigma_h / math.sqrt(3):
                 hidden_states = self.rescale(normed_dw, self.sigma_h)
