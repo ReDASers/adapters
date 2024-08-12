fix(lora): adjust sigma_h calculation and rescaling conditions
Adjusts the condition for weight rescaling to check at the start of an epoch. Also, ensures sigma_h is capped by sigma_w divided by the number of batches. These changes improve stability during training by refining how standard deviations are handled.

src/adapters/methods/lora.py
@@ -485,7 +485,7 @@ class LoRA(nn.Module):
             if self._epoch_end():
                 self.sigma_w = (self.sigma_w / self.batches_per_epoch)
                 
-        if self.epoch > 1 and weights.std().item() > self.sigma_w:
+        if self._epoch_start() and self.epoch > 1 and weights.std().item() > self.sigma_w:
             w = self.rescale(weights, 
                             sigma=self.sigma_w, 
                             noise_std=self.noise_std, 
@@ -543,6 +543,7 @@ class LoRA(nn.Module):
             if self.training and self.epoch == 1:
                 self.batch_sigmas[self.n_batches - 1] = sigma_dw 
                 self.sigma_h = torch.mean(self.batch_sigmas).item()
+                self.sigma_h = min(self.sigma_h, self.sigma_w/self.n_batches)
                  
             # Rescale delta_w if its standard deviation is greater than sigma_h
             if sigma_dw > self.sigma_h: