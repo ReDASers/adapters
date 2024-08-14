d9bd1b0bdb760ca5e43786c39361f2175373fa03
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-14 21:15:14 +0000

    rr

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -484,10 +484,10 @@ class LoRA(nn.Module):
                     
             if self._epoch_end():
                 self.sigma_w = (self.sigma_w / self.batches_per_epoch)
-        if self.training and self.epoch > 1:
+        if self.training and self.epoch > 1 and self._epoch_start():
             w = self.rescale(weights=weights, 
                              sigma=self.sigma_w, 
-                             skip_prob=self.pW, 
+                             skip_prob=0.0, 
                              weight_dropout_prob=self.weight_dropout_prob)
         else:
             w = weights
