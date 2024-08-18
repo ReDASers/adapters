caa41f878950f77c687571aeb5317cc1de31289a
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-18 08:51:08 +0000

    fix(lora): adjust dropout probability in training mode
    Increased the dropout probability from 0.5 to 0.8 when the location is "output" and the model is in training mode with epoch > 1. This change aims to improve regularization during training.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -488,7 +488,7 @@ class LoRA(nn.Module):
                
         if self.training and self.epoch > 1:
             if self.location == "output":
-                p = 0.5
+                p = 0.8
             else:
                 p = 1 - self.p
             w = self.rescale(weights=weights, 
