fix(lora): correct noise injection scaling in _inject_noise method
Adjusted the noise injection in the `_inject_noise` method by introducing a variable `s` to store the standard deviation of weights. This change ensures consistent and accurate scaling of noise, improving the stability and performance of the LoRA model.

src/adapters/methods/lora.py
@@ -449,11 +449,12 @@ class LoRA(nn.Module):
                                   weight_dropout_prob=weight_dropout_prob)
     
     def _inject_noise(self, weights: torch.Tensor, noise_std: float = 0.01) -> torch.Tensor:
+        s = weights.std().item()
         return self._rescale(weights=weights, 
-                             sigma=torch.normal(
+                             sigma=s + torch.normal(
                                 mean=0.0, 
-                                std=noise_std * weights.std().item(), 
-                                size=(), 
+                                std=noise_std * s, 
+                                size=(1,), 
                                 dtype=weights.dtype, 
                                 device=weights.device,
                                 ).item(),