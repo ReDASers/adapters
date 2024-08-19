fix(lora): correct skip_prob calculation for non-output locations
Adjusted the probability calculation in the LoRA class to ensure proper rescaling of weights when location is not "output". This change ensures consistency and correctness in model behavior during training.

src/adapters/methods/lora.py
@@ -492,7 +492,7 @@ class LoRA(nn.Module):
                 elif self.location == "output":
                     p = 1 - 1/self.batches_per_epoch
                 else:
-                    p = 0.0
+                    p = 1/self.batches_per_epoch
                 w = self.rescale(weights=weights, 
                                 sigma=self.sigma_w, 
                                 skip_prob=p,