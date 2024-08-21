67bb027e9af52ca7f100b02cf2f24ae46e6b3ab8
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-21 01:09:25 +0000

    fix(lora): correct standard deviation calculation for parameter initialization
    Adjusted the standard deviation calculation in the LoRA class initialization to use the square of (1/self.batches_per_epoch) instead of its square root. This ensures more accurate parameter initialization, potentially improving model stability and performance.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -110,7 +110,9 @@ class LoRA(nn.Module):
             nn.init.zeros_(self.lp.bias)
             '''
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch))
-            nn.init.normal_(self.p, mean=1 - 1/self.batches_per_epoch, std=math.sqrt(1/self.batches_per_epoch))
+            nn.init.normal_(self.p, 
+                            mean=1 - 1/self.batches_per_epoch,
+                            std=(1/self.batches_per_epoch)**2)
         
 
         
