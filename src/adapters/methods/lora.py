b72039644762adb2da2e2b0b78065771b9be10f6
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-22 18:38:00 +0000

    fix(lora): correct parameter initialization bounds in LoRA class
    Adjusted the parameter initialization to use uniform distribution instead of normal distribution and corrected lower bound calculation. This ensures more accurate parameter initialization within specified limits, improving model stability.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -106,9 +106,9 @@ class LoRA(nn.Module):
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             self.mu = 1 - 1/self.batches_per_epoch
             self.std = math.sqrt(2*self.mu/self.connections_out)
-            #nn.init.uniform_(self.p, a=max(0,self.mu-self.std*math.sqrt(3)), b=min(1,self.mu+self.std*math.sqrt(3)))
-            nn.init.normal_(self.p, mean=self.mu, std=self.std)
-            self.lbound = self.mu - self.std * 5
+            nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.std*math.sqrt(3)), b=min(1.0,self.mu+self.std*math.sqrt(3)))
+            #nn.init.normal_(self.p, mean=self.mu, std=self.std)
+            self.lbound = max(1 - p, self.mu - math.sqrt(3)*self.std * 5)
             self.ubound = 1.0
         
 
