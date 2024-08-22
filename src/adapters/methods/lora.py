fix(lora): correct standard deviation and bounds calculation
Updated the standard deviation formula to use the maximum of connections_out and batches_per_epoch, ensuring proper scaling. Adjusted lower bound calculation to improve stability by using 5 times the standard deviation instead of 2.

src/adapters/methods/lora.py
@@ -105,10 +105,10 @@ class LoRA(nn.Module):
         else:
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             self.mu = 1 - 1/self.batches_per_epoch
-            self.std = math.sqrt(2*self.mu/self.connections_out) # out is just number of neurons for scaling vector
+            self.std = math.sqrt(2/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
             #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit))
             nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
-            self.lbound = max(0, 1 - p, self.mu - 2*self.std * 2)
+            self.lbound = max(0, 1 - p, self.mu - self.std * 5)
             self.ubound = 1.0
         
 