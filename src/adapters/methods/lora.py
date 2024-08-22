fix(lora): correct lower bound calculation in initialization
Revised the lower bound calculation in the LoRA class to use a factor of 2 instead of sqrt(3) for standard deviation scaling. This adjustment aims to improve the accuracy and stability of parameter initialization.

src/adapters/methods/lora.py
@@ -108,7 +108,7 @@ class LoRA(nn.Module):
             self.std = math.sqrt(2*self.mu/self.connections_out) # out is just number of neurons for scaling vector
             #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit))
             nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=max(1 - p, self.mu-2*self.std), b=min(1.0,self.mu+2*self.std)) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
-            self.lbound = max(1 - p, self.mu - 2*self.std * math.sqrt(3))
+            self.lbound = max(1 - p, self.mu - 2*self.std * 2)
             self.ubound = 1.0
         
 