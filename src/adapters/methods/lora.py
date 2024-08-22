fa573604c61158fd9ac4d808fba877de1b0d2494
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-22 19:53:22 +0000

    lecun

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -105,10 +105,10 @@ class LoRA(nn.Module):
         else:
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             self.mu = 1 - 1/self.batches_per_epoch
-            self.std = math.sqrt(self.mu/self.connections_out)
-            nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.std*math.sqrt(3)), b=min(1.0,self.mu+self.std*math.sqrt(3)))
+            self.limit = math.sqrt(3*self.mu/self.connections_out)
+            nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit))
             #nn.init.normal_(self.p, mean=self.mu, std=self.std)
-            self.lbound = max(1 - p, self.mu - math.sqrt(3)*self.std * 5)
+            self.lbound = max(1 - p, self.mu - self.limit * 5)
             self.ubound = 1.0
         
 
