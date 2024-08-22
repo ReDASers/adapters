fix(lora): adjust epoch scaling factor in weight rescaling
Adjusted the scaling factor for epochs in the weight rescaling function within the LoRA class. This change ensures more accurate clamping of skip probabilities, improving model stability and performance during training.

src/adapters/methods/lora.py
@@ -505,8 +505,8 @@ class LoRA(nn.Module):
                 w = self.rescale(weights=weights, 
                                 sigma=self.sigma_w, 
                                 skip_prob=torch.clamp(self.p, 
-                                                      min=max(math.sqrt(self.std), self.mu - self.std*math.sqrt(3) -self.std*self.epoch),
-                                                      max=min(self.mu+self.std*math.sqrt(3)+self.std*self.epoch, 1 - 1e-6),
+                                                      min=max(math.sqrt(self.std), self.mu - self.std*math.sqrt(3) - 0.5*self.std*self.epoch),
+                                                      max=min(self.mu+self.std*math.sqrt(3)+0.5*self.std*self.epoch, 1 - 1e-6),
                                                       ) if self.location != "selfattn" else torch.clamp(self.p, min=0.0, max=1.0),
                                 weight_dropout_prob=self.weight_dropout_prob)
                 