fix(lora): adjust standard deviation calculation in LoRA module
Changed the scaling factor in the standard deviation calculation from 6 to 5 for better accuracy in initializing parameters. This adjustment impacts the distribution range of initialized parameters, potentially improving model performance.

src/adapters/methods/lora.py
@@ -105,7 +105,7 @@ class LoRA(nn.Module):
         else:
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             pepoch = 1 - 1/self.batches_per_epoch
-            self.std = math.sqrt((6*pepoch)/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
+            self.std = math.sqrt((5*pepoch)/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
             self.mu = pepoch+1/(2*self.batches_per_epoch) - self.std
 
             nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))