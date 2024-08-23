29e7de0eb7e3d087cce2df86bd3846d1fbe246d4
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-23 03:13:46 +0000

    refactor(lora): remove commented-out initialization code
    Removed unnecessary commented-out code for parameter initialization in the LoRA class to improve code readability and maintainability.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -109,7 +109,7 @@ class LoRA(nn.Module):
             self.mu = pepoch+1/(2*self.batches_per_epoch) - self.std
 
             nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))
-            ##nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
+            
             self.lbound = max(0, self.mu - 2 * self.std)
             self.ubound = min(1.0, max(self.lbound, 1-(1/self.batches_per_epoch)*p, min(1.0,self.mu+self.std+1/(math.sqrt(3)*self.batches_per_epoch))))
         
