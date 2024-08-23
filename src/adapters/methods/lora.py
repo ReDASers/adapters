c1775f76252903ef0d30ca83a26af207308b5e81
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-23 00:57:12 +0000

    refactor(lora): streamline parameter initialization
    Simplified the initialization of `mu` and `std` by removing redundant calculations. This improves code readability and maintains consistent logic for setting bounds on the parameter `p`.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -104,15 +104,14 @@ class LoRA(nn.Module):
             self.ubound = 1.0
         else:
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
-            self.mu = 1 - 1/self.batches_per_epoch
-            self.std = math.sqrt((3*self.mu)/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
-            self.mu -= self.std
+            pepoch = 1 - 1/self.batches_per_epoch
+            self.std = math.sqrt((3*pepoch)/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
+            self.mu = min(pepoch, 1 - self.std)
+
             nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))
             ##nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
             self.lbound = max(0, self.mu - self.std * math.sqrt(3))
-            self.ubound = min(1.0,self.mu+self.std+1/(self.batches_per_epoch*math.sqrt(3)))
-        
-
+            self.ubound = min(1.0,self.mu+self.std)
         
     def _calculate_batches_per_epoch(self, batch_size: Optional[int], training_set_size: Optional[int]) -> int:
         """
