fix(lora): adjust upper bound calculation for parameter initialization
Adjusted the upper bound calculation in the LoRA class to account for batch size by adding a term dependent on batches per epoch. This change ensures more accurate initialization of parameters, potentially improving model performance and stability.

src/adapters/methods/lora.py
@@ -110,7 +110,7 @@ class LoRA(nn.Module):
             nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))
             ##nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
             self.lbound = max(0, self.mu - self.std * 2)
-            self.ubound = 1.0
+            self.ubound = min(1.0,self.mu+self.std+1/(self.batches_per_epoch*math.sqrt(3)))
         
 
         