af0b27efe976b556fb8d474fbcd0c2fc843a955a
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-25 10:40:59 +0000

    limit = var, min = mu-limit*sqrt(5)

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -110,10 +110,10 @@ class LoRA(nn.Module):
             pepoch = 1/self.batches_per_epoch - self.eps
             var = pepoch * math.sqrt(math.sqrt(6/(self.connections_out))) # out is just number of neurons for scaling vector
             mu =  1 - pepoch
-            limit = var/2 * math.sqrt(3)
+            limit = var
 
             nn.init.uniform_(self.p, a=max(0, mu-limit), b=min(1.0, mu+limit))
-            self.lbound = max(0, mu - limit)
+            self.lbound = max(0, mu - limit * math.sqrt(5))
             self.ubound = min(1.0, mu + limit)
         
     def _calculate_batches_per_epoch(self, batch_size: Optional[int], training_set_size: Optional[int]) -> int:
