519f454f9b01c23e93a816b71260e7eb08f58ab7
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-25 06:17:16 +0000

    avoid possible division by 0 when 1 epoch is 1 traininig batch

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -106,12 +106,11 @@ class LoRA(nn.Module):
         else:
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             pepoch = 1/self.batches_per_epoch
-            std = math.sqrt((6*(1 - pepoch))/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
-            mu =  1 - pepoch
-            limit = min(pepoch*0.99, std)
+            var = math.sqrt(math.sqrt(6/(self.connections_out + self.batches_per_epoch))) # out is just number of neurons for scaling vector
+            mu =  1 - pepoch + 1e-6
+            limit = pepoch * var + 1e-6
 
             nn.init.uniform_(self.p, a=max(0, mu-limit), b=min(1.0,mu+limit))
-            #nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
             self.lbound = max(0, mu - limit)
             self.ubound = min(1.0, mu + limit)
         
