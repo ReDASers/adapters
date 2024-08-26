3bd2fb52901b0294f3eae5048f485a38a9a18edf
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-26 07:16:26 +0000

    lbound ubound calc change

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -108,7 +108,7 @@ class LoRA(nn.Module):
             self.lbound = 0.0
             self.ubound = 1.0
         else:
-            self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
+            self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch,requires_grad=True, dtype=torch.float32))
             pepoch = 1/self.batches_per_epoch - torch.finfo(torch.float32).eps
             var = math.sqrt(math.sqrt(6/self.connections_out)) # out is just number of neurons for scaling vector
             mu =  1 - pepoch
@@ -116,8 +116,8 @@ class LoRA(nn.Module):
 
             nn.init.uniform_(self.p, a=max(0, mu-limit), b=min(1.0,mu+limit))
             #nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
-            self.lbound = max(0, mu - limit*self.slacka)
-            self.ubound = min(1.0, mu + limit*self.slackb)
+            self.lbound = max(0, mu - var*pepoch*self.slacka)
+            self.ubound = min(1.0, mu + var*pepoch*self.slackb)
         
     def _calculate_batches_per_epoch(self, batch_size: Optional[int], training_set_size: Optional[int]) -> int:
         """
