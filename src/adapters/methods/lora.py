0ebd25c0afecc5a870085de459ae9f39858dffac
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-27 04:33:45 +0000

    controls added

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -113,11 +113,13 @@ class LoRA(nn.Module):
             pepoch = 1 - 1/self.batches_per_epoch
             self.std = math.sqrt((3*pepoch)/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
             # self.mu = pepoch+1/(2*self.batches_per_epoch) - self.std
-            self.mu = pepoch-1e-6
+            c = (1/self.batches_per_epoch)*self.learning_multiplier-(1/self.batches_per_epoch)
+            self.mu = pepoch-1e-6+c
+            
             ##nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))
-            nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
-            self.lbound = max(0, self.mu - self.std)
-            self.ubound = min(1.0,self.mu + self.std)
+            nn.init.normal_(self.p, mean=self.mu, std=self.std*self.a) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
+            self.lbound = max(0, self.mu - self.std*self.slacka)
+            self.ubound = min(1.0,self.mu + self.std*self.slackb)
 
             '''
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
