738ee6f27313111aba176657fdaa0c32c729d2e5
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-27 00:27:07 +0000

    limits 099

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -109,6 +109,17 @@ class LoRA(nn.Module):
             self.ubound = 1.0
         else:
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
+            pepoch = 1/self.batches_per_epoch
+            std = math.sqrt((6*(1 - pepoch))/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
+            mu =  1 - pepoch
+            limit = min(pepoch*0.99, std)
+
+            nn.init.uniform_(self.p, a=max(0, mu-limit), b=min(1.0,mu+limit))
+            #nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
+            self.lbound = max(0, mu - limit)
+            self.ubound = min(1.0, mu + limit)
+            '''
+            self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             pepoch = 1 - 1/self.batches_per_epoch
             self.std = math.sqrt((6*pepoch)/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
             self.mu = pepoch+1/(2*self.batches_per_epoch) - self.std
@@ -117,7 +128,8 @@ class LoRA(nn.Module):
             #nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
             self.lbound = max(0, self.mu - self.std)
             self.ubound = min(1.0,self.mu + self.std)
-            '''
+            
+
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             pepoch = 1 - 1/self.batches_per_epoch
             self.std = math.sqrt((6*pepoch)/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
