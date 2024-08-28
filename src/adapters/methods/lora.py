0b75b32d38d19fc84aee6c736566bb3995d5f0f3
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-28 07:11:29 +0000

    upd

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -112,7 +112,7 @@ class LoRA(nn.Module):
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             
             mu =  1 - (1/(2*self.batches_per_epoch)) * self.a
-            std = (1/(4*self.batches_per_epoch))*self.a
+            std = (1/(4*self.batches_per_epoch))*self.a * self.learning_multiplier
             # self.std = m # out is just number of neurons for scaling vector
             # self.mu = pepoch+1/(2*self.batches_per_epoch) - self.std
             
@@ -121,8 +121,8 @@ class LoRA(nn.Module):
             
             ##nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))
             nn.init.normal_(self.p, mean=mu, std=std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
-            self.lbound = max(0, mu - std*self.slacka)
-            self.ubound = min(1.0,mu + std*self.slackb)
+            self.lbound = 0.0
+            self.ubound = 1.0
 
             '''
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
