678614bafd4bcf9bf827007bf8349bb7dadb7756
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-28 12:43:59 +0000

    best before

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -110,6 +110,17 @@ class LoRA(nn.Module):
         else:
 
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
+            pepoch = 1 - 1/self.batches_per_epoch
+            self.std = math.sqrt((6*pepoch)/(max(self.connections_out, self.batches_per_epoch))) # out is just number of neurons for scaling vector
+            self.mu = pepoch+1/(2*self.batches_per_epoch)*self.a - self.std*self.load_state_dict
+
+            nn.init.uniform_(self.p, a=max(0, self.mu-self.std), b=min(1.0,self.mu+self.std))
+            ##nn.init.normal_(self.p, mean=self.mu, std=self.std) #nn.init.uniform_(self.p, a=max(1 - p, self.mu-self.limit), b=min(1.0,self.mu+self.limit)) #nn.init.trunc_normal_(self.p, mean=self.mu, std=self.std, a=self.mu-2*self.std, b=self.mu+2*self.std)
+            self.lbound = max(0, self.mu - self.std*self.slacka)
+            self.ubound = min(1.0,self.mu+self.std+(1/(math.sqrt(3)*self.batches_per_epoch))*self.slackb)
+        
+
+            '''
             mu = 1 - pow(0.5, 1/self.batches_per_epoch)
             mu =  1 - (1/(2*self.batches_per_epoch)) * self.a
             std = (1/(10*self.batches_per_epoch)) * self.learning_multiplier
@@ -124,7 +135,7 @@ class LoRA(nn.Module):
             self.lbound = 0.0
             self.ubound = 1.0
 
-            '''
+            
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             pepoch = 1/self.batches_per_epoch
             std = math.sqrt((6*(1 - pepoch))/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
