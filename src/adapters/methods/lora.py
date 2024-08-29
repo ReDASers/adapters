fix(lora): adjust upper bound calculation for stability
In the LoRA class, modified the calculation of `self.ubound` to use `mu + m*1.5` instead of `mu + m`. This change aims to improve the stability of the upper bound value during training.

src/adapters/methods/lora.py
@@ -109,7 +109,7 @@ class LoRA(nn.Module):
             mu = pepoch-1e-6
             self.lbound = max(0, mu - std)
             m = (1 - mu)/ 2
-            self.ubound = min(1.0, mu + m, mu + std)
+            self.ubound = min(1.0, mu + m*1.5, mu + std)
 
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             nn.init.normal_(self.p, mean=mu, std=std)