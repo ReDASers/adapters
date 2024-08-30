fix(lora): adjust bounds calculation for parameter initialization
Updated the lower and upper bounds calculation in LoRA class to consider both 'm' and 'std' values. This change ensures more accurate parameter initialization by refining the range within which parameters are truncated.

src/adapters/methods/lora.py
@@ -98,8 +98,8 @@ class LoRA(nn.Module):
             std = math.sqrt((3*pepoch)/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
             mu = pepoch-1e-6
             m = ((1.0 - mu) / 2)*1.5
-            self.lbound = max(0, mu - std)
-            self.ubound = min(1.0, mu + std)
+            self.lbound = max(0, mu - m, mu - std)
+            self.ubound = min(1.0, mu+m, mu + std)
 
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             nn.init.trunc_normal_(self.p, mean=mu, std=std, a=self.lbound, b=self.ubound)