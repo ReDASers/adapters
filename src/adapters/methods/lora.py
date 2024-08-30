fix(lora): correct lower bound calculation in LoRA method
Adjusted the lower bound calculation to use standard deviation directly instead of scaling it by 1.5. This change ensures more accurate parameter initialization and potentially improves model performance.

src/adapters/methods/lora.py
@@ -98,7 +98,7 @@ class LoRA(nn.Module):
             std = math.sqrt((3*pepoch)/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
             mu = pepoch-1e-6
             m = ((1.0 - mu) / 2)
-            self.lbound = max(0, mu - std/2*1.5)
+            self.lbound = max(0, mu - std)
             self.ubound = min(1.0, mu+m*1.5, mu + std)
 
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))