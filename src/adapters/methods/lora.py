9ec18780bad6a1148c725f0a19b4c3f6810ae449
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-30 04:15:13 +0000

    fix(lora): adjust upper bound calculation in LoRA class
    Adjusted the upper bound calculation to use 1.5 times the midpoint value. This change ensures more accurate parameter initialization and better model performance by refining the boundary constraints.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -110,7 +110,7 @@ class LoRA(nn.Module):
             m = (1.0 - mu) / 2
             self.lbound = max(0, mu - std)
             
-            self.ubound = min(1.0, mu + m, mu + std)
+            self.ubound = min(1.0, mu + m*1.5, mu + std)
 
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             nn.init.normal_(self.p, mean=mu, std=std)
