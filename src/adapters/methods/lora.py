4baa7b7441c33ac0adafe4a454ca0f15a3951cc3
Dainis Boumber <dainis.boumber@gmail.com>
2024-08-30 15:38:36 +0000

    fix(lora): correct lower bound calculation in LoRA class
    Adjusted the calculation of the lower bound (lbound) in the LoRA class to use a consistent scaling factor for standard deviation. This change ensures more accurate parameter initialization and potentially better model performance.

--- src/adapters/methods/lora.py
+++ src/adapters/methods/lora.py
@@ -98,7 +98,7 @@ class LoRA(nn.Module):
             std = math.sqrt((3*pepoch)/(self.connections_out + self.batches_per_epoch)) # out is just number of neurons for scaling vector
             mu = pepoch-1e-6
             m = ((1.0 - mu) / 2)*1.5
-            self.lbound = max(0, mu - m, mu - std)
+            self.lbound = max(0, mu - std*1.5)
             self.ubound = min(1.0, mu+m, mu + std)
 
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
