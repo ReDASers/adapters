fix(lora): adjust upper bound to prevent overflow
Adjusted the upper bound value in the LoRA class to 1.0 - 5e-3 to prevent potential overflow issues during training. This minor change ensures better numerical stability and consistency in the model's parameter initialization.

src/adapters/methods/lora.py
@@ -101,7 +101,7 @@ class LoRA(nn.Module):
             std = 1 / math.sqrt(self.connections_out)
             #std = math.sqrt((3*mu)/(self.connections_out+self.batches_per_epoch)) # out is just number of neurons for scaling vector
             self.lbound =  max(0, mu - math.sqrt(5)*std)
-            self.ubound =  1.0 # min(1.0, mu+0.99*pepoch, mu+std)
+            self.ubound =  1.0 - 5e-3 # min(1.0, mu+0.99*pepoch, mu+std)
             
             self.p = nn.Parameter(torch.tensor(1 - 1/self.batches_per_epoch, dtype=torch.float32))
             nn.init.trunc_normal_(self.p, mean=mu, std=std, a=max(0, mu - 2*std), b=min(1.0, mu+0.75*pepoch, mu+std))